5a68f31a88d6b8d5b7af93ff545cab9a2c1663a0
Francisco Morales <126733326+Framor81@users.noreply.github.com>
2023-06-21 14:44:44 -0700

    Initial Python OSC Utility
    Co-authored-by: anjalinugg4 <anjalinugg4@users.noreply.github.com>

--- demo.py
+++ demo.py
@@ -5,5 +5,7 @@ import ue5osc
 osc_sender = ue5osc.OSCSender("127.0.0.1", 7447)
 
 for i in range(10):
-    osc_sender.move_forward(i)
+    osc_sender.move_forward(98.0)
     sleep(0.1)
+
+    
--- ue5osc/__init__.py
+++ ue5osc/__init__.py
@@ -8,7 +8,15 @@ class OSCSender:
     def move_forward(self, amount: float):
         self.client.send_message("/move/forward", float(amount))
 
-    # Turn right
-    # Turn left
-    # Take screenshot
-    # Reset to beginning
+    def turn_left(self, degree: float):
+        self.client.send_message("/turn/left", float(degree))
+
+    def turn_right(self, degree: float):
+        self.client.send_message("/turn/right", float(degree))
+
+    #float parameters are optional for these two functions
+    def take_screenshot(self, degree: float=0.0):
+        self.client.send_message("/take/screenshot", float(degree))
+
+    def reset_to_start(self, degree: float=0.):
+        self.client.send_message("/return/start", float(degree))
