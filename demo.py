e9a308621c0e9b611d38931a40ab24d31c3d57be
Chau Vu <vuhachau2412@gmail.com>
2023-07-14 00:24:52 +0700

    Updated demo.py's main function to remove path argument

--- demo.py
+++ demo.py
@@ -8,7 +8,6 @@ def main():
     """Argument Parser that verifies that the image path is getting passed in and
     optional ability to set ip and ports."""
     parser = ArgumentParser()
-    parser.add_argument("path", type=str, help="Path to folder to store images.")
     parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP Address")
     parser.add_argument("--client_port", type=int, default=7447, help="Client Port")
     parser.add_argument("--server_port", type=int, default=7001, help="Server Port")
@@ -16,7 +15,7 @@ def main():
     args = parser.parse_args()
 
     with ue5osc.Communicator(
-        args.ip, args.client_port, args.server_port, args.path
+        args.ip, args.client_port, args.server_port, 
     ) as osc_communicator:
         print(osc_communicator.get_location()[0])
         sleep(1)
